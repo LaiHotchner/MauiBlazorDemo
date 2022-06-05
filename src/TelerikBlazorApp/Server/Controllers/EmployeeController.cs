37a6efc8ac4a8112fd4dff301ea569cdc903eaed
Hotchner Lai <hotchner.lai@thermofisher.com>
2022-06-05 15:26:25 +0800

    Add webassembly project

---
+++ src/TelerikBlazorApp/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikBlazorApp.Server.Data;
+using TelerikBlazorApp.Shared.Models.Employee;
+
+namespace TelerikBlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+        public EmployeeController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var result = _dataService.GetEmployees().ToList();
+            return result;
+        }
+    }
+}
