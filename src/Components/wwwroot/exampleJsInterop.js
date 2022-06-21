7d18dcc8c588e31db1f9d45c756b155c542bf44a
Hotchner <xiaofeng.lai@yinfantech.com>
2022-06-21 21:25:19 +0800

    Implate arcgis for javascript in Web client

---
+++ src/Components/wwwroot/exampleJsInterop.js
@@ -0,0 +1,17 @@
+﻿window.renderMap = () => {
+    require(["esri/config", "esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+
+        esriConfig.apiKey = "AAPK6feb7f74e7304f62967a618d0fa6a76dBooXKnQZy3T9ZkJ7WqH__hgOsQxT0w8kBcJCx53FUQkde1mrp7uwSeBrzgCL8m61";
+
+        const map = new Map({
+            basemap: "arcgis-topographic" // Basemap layer service
+        });
+
+        const view = new MapView({
+            map: map,
+            center: [-118.805, 34.027], // Longitude, latitude
+            zoom: 13, // Zoom level
+            container: "view-div" // Div element
+        });
+    });
+};
